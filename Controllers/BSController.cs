14ebdbe70ede3722bc3b9060d014e3314541dec5
MRGDFK <sharjilskhan3771@gmail.com>
2024-07-01 03:40:07 +0600

    Buy Page - Eftee
    - Buy Page Front End Done!

--- Controllers/BSController.cs
+++ Controllers/BSController.cs
@@ -94,5 +94,22 @@ namespace UrbanNest.Controllers
 
             return View(model);
         }
+
+        public ActionResult Buy()
+        {
+            var properties = GetPropertiesFromDatabase();
+            return View(properties);
+        }
+
+        private IEnumerable<property> GetPropertiesFromDatabase()
+        {
+            // This is just a placeholder. Replace it with your database fetching logic.
+            return new List<property>
+            {
+                new property { Title = "Beautiful Family House", Location = "BADDA", Price = "350000", Area = "2500 sqft", Bed ="3", Bath="4", status="on sell", Image_01 ="prop1.jpeg"},
+
+            };
+        }
+
     }
 }
