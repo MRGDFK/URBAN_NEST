f81762d0b37078395d749015a357ee7a518a9ca9
Ahammed Aoual <ahammed.aoual@gmail.com>
2024-07-01 02:25:51 +0600

    initial commit

---
+++ Models/RealEstateContext.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class RealEstateContext : DbContext
+{
+    public DbSet<UserProfile> UserProfiles { get; set; }
+    public DbSet<RealEstateListing> RealEstateListings { get; set; }
+}
+
